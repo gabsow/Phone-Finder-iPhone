0ff133e49c0a207a77a63aa945d013626a6a26ed
tom gabsow <tommac@toms-Mac-mini.local>
2014-06-13 00:01:51 +0300

    Initial Commit

---
+++ Phone Finder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Phone Finder
+//
+//  Created by tom gabsow on 6/13/14.
+//  Copyright (c) 2014 GABSOW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Phone Finder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Phone Finder
+//
+//  Created by tom gabsow on 6/13/14.
+//  Copyright (c) 2014 GABSOW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
