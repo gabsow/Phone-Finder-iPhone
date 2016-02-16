a73182c3356b3c567a6d9a6e57ef2f892bc7c22f
tom gabsow <tommac@toms-Mac-mini.local>
2016-02-17 00:58:06 +0200

    ios 7 bug

--- Phone Finder/ViewController.h
+++ Phone Finder/ViewController.h
@@ -7,7 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+// 1. Import the gamekit framework
+#import <GameKit/GameKit.h>
 
-@interface ViewController : UIViewController
+// 2. Subclass to GKSessionDelegate and GKPeerPickerControllerDelegate
+//    GKSessionDelegate - Used to maintain Sessions
+//    GKPeerPickerControllerDelegate - Gives a apple provided peer picker,
+//            where you can look for other devcies using teh same apps to connect with
 
+
+@interface ViewController : UIViewController<GKSessionDelegate, GKPeerPickerControllerDelegate>{
+	
+    // 3. Create these folloeing for
+	
+	// Session Object
+	//GKSession *Session;
+	// PeerPicker Object
+	GKPeerPickerController *picker;
+	// Array of peers connected
+	//NSMutableArray *Peers;
+}
+
+
+@property (nonatomic,retain) IBOutlet UILabel *hellowLabele;
+- (IBAction)sayHello:(id)sender;
 @end
--- Phone Finder/ViewController.m
+++ Phone Finder/ViewController.m
@@ -7,23 +7,36 @@
 //
 
 #import "ViewController.h"
-
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+@synthesize hellowLabele;
 
+- (IBAction)sayHello:(id)sender{
+    hellowLabele.text = @"hello to gabsow";
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    // setup bluetooth interface
+    //btManager = [BluetoothManager sharedInstance];
+    
+    NSLog(@"view did log");
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(IBAction) btnConnect:(id) sender {
+    picker = [[GKPeerPickerController alloc] init];
+    picker.delegate = self;
+    picker.connectionTypesMask = GKPeerPickerConnectionTypeNearby;
+    [picker show];
 }
 
 @end
